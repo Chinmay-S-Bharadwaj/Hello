c16f819e7e4f50a7295c7cf4e27990c7e47a70e5
Chinmay S Bharadwaj <stratelworld@gmail.com>
2026-08-19 10:45:35 +0530

    This is the voting criteria

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ function greet(name) { return 'Hello, ' + name; }
 Hello World
 print(Hello World)
 print("Hello World")
+print("Hello World")
+if age > 18:
+print("You are eligible to vote")
+else:
+print("You are not eligible to vote")
